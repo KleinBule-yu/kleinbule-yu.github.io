aa33925d34f63a65391d398dfe0ba02214b003db
KleinBule-yu <yzy20070722yzy@qq.com>
2026-03-19 13:08:09 +0800

    Site updated: 2026-03-19 13:08:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/10/hello-world/","2026/03/18/Windows/","2026/03/11/YUM源软件仓库/","2026/03/18/Linux/"];function toRandomPost(){
+var posts=["2026/03/18/Windows/","2026/03/11/YUM源软件仓库/","2026/03/10/hello-world/","2026/03/18/Linux/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
