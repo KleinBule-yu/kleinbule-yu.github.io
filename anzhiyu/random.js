fcbded70e93ed28944b37d846a0fed5f2dc7eefc
KleinBule-yu <yzy20070722yzy@qq.com>
2026-03-23 13:45:29 +0800

    Site updated: 2026-03-23 13:45:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/19/Linux基础命令/","2026/03/23/硬链接和软链接/","2026/03/20/绝对路径和相对路径/","2026/03/10/hello-world/","2026/03/11/YUM源软件仓库/","2026/03/19/Linux系统文件结构/"];function toRandomPost(){
+var posts=["2026/03/19/Linux基础命令/","2026/03/19/Linux系统文件结构/","2026/03/10/hello-world/","2026/03/11/YUM源软件仓库/","2026/03/23/文件目录权限属性/","2026/03/23/硬链接和软链接/","2026/03/20/绝对路径和相对路径/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
